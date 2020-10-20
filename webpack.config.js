c424865b3822c04f3c7045e1f7d54f30a66b6b83
petemcnie <petemcnie0@gmail.com>
2020-10-21 12:32:02 +1300

    Updated entry point filename and rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path')
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.tsx',
   output: {
     path: path.join(__dirname, 'server/public'),
     filename: 'bundle.js'
   },
   mode: 'development',
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
